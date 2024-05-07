bb611ba06bb1972c48ea8c61bb16b4b6872b4320
eceh-enmu <eduardo.ceh.enmu@gmail.com>
2024-05-07 15:02:17 -0600

    Created the Client class

---
+++ Part3/src/Client.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author CEHVAREE
+ */
+public class Client {
+    
+    private String name;
+    
+    public Client(String name)
+    {
+        this.name = name;
+    }
+    
+}
